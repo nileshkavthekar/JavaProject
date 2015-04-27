24728f264bf6acce510542763d7e59c227c94363
nileshkavthekar <nilesh.kavthekar@accenture.com>
2015-04-27 12:33:35 +0530

    Canvas module test

--- TestConcepts/src/com/canvas/demo/CanvasModuleTest.java
+++ TestConcepts/src/com/canvas/demo/CanvasModuleTest.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CanvasTest {
+public class CanvasModuleTest {
 	public static void main(String args[])
 	{
 		String canvasDetails[][];
@@ -17,10 +17,26 @@ public class CanvasTest {
 		    System.out.println(inputLine);
 		    String inputDetails[]= inputLine.trim().split(" ");
 		  
+		
 		   if(inputDetails[0].equals("C"))
 		   {
 			   components.drawCanvas(new Canvas(Integer.parseInt(inputDetails[1]), Integer.parseInt(inputDetails[2])));
 		   }
+		   components.showCanvas();
+		    System.out.println("Enter Line Details");
+		    inputLine = bufferRead.readLine();			 
+		    System.out.println(inputLine);
+		    inputDetails= inputLine.trim().split(" ");
+		   
+		    if(inputDetails[0].equals("L"))
+			 {
+			  components.drawLine(new Line(Integer.parseInt(inputDetails[1].trim()), Integer.parseInt(inputDetails[2].trim()),
+					  Integer.parseInt(inputDetails[3].trim()), Integer.parseInt(inputDetails[4].trim())));
+			 }
+		    
+		    components.showCanvas();
+		    
+		    
 		    System.out.println("Enter Line Details");
 		    inputLine = bufferRead.readLine();			 
 		    System.out.println(inputLine);
